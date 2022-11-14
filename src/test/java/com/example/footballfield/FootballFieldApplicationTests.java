f556b0135ae30f214cfe899150b725a4486fc616
fabia <fhoefler@htl-steyr.ac.at>
2022-11-14 13:47:16 +0100

    Initial commit

---
+++ src/test/java/com/example/footballfield/FootballFieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballfield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballFieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
