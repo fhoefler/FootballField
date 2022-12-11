841caada61c7694f46071dcd9fa99dfcf5793675
fabia <fhoefler@htl-steyr.ac.at>
2022-12-11 19:31:22 +0100

    Test for Appiontment Field and Team

--- src/test/java/com/example/footballfield/FootballFieldApplicationTests.java
+++ src/test/java/com/example/footballfield/FootballFieldApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.footballfield;
 
 import htl.steyr.model.Appiontment;
+import htl.steyr.model.Field;
 import htl.steyr.model.Teams;
 import htl.steyr.model.repository.AppointmentRepository;
 import htl.steyr.model.repository.FieldRepository;
@@ -13,8 +14,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,13 +26,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FootballFieldApplicationTests {
 
 
+    @Test
+    void loadTeams() throws Exception {
+        Teams teams = new Teams("Aschach");
+
+        Assertions.assertEquals("Aschach", teams.getName());
+    }
+
+    @Test
+    void loadField() throws Exception {
+        Field field = new Field("Aschach");
+
+        Assertions.assertEquals("Aschach", field.getName());
+    }
 
 
     @Test
-    void loadTeams() throws Exception {
-            Teams teams = new Teams("Aschach");
+    void loadAppiontment() throws Exception {
+        Field field = new Field("Aschach");
+        String date = "2022-12-05 14:33:04";
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Appiontment appiontment = new Appiontment(formatter.parse(date), field);
 
-            Assertions.assertEquals("Aschach", teams.getName());
-        }
+        Assertions.assertEquals("Aschach", appiontment.getField().getName());
+        Assertions.assertEquals(formatter.parse("2022-12-05 14:33:04"), appiontment.getDate());
+    }
 
 }
